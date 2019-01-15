7ea0ada4bda5ec02cb6c481e04ff6e2d06dc7cca
Mansoor767 <a@gmail.com>
2019-01-15 11:06:13 +0530

    menu button, nav, news img scale

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Toggle main menu
+var menuBtn = document.getElementById("menuBtn");
+var topHeader = document.getElementById("topHeader");
+
+menuBtn.addEventListener('click', function(){
+  if(topHeader.classList.contains('active')) {
+    topHeader.classList.remove('active');
+  } else {
+    topHeader.classList.add('active');
+  }
+})
+
+// Toggle toTop button
+var toTopBtn = document.getElementById("toTopBtn");
+
+window.addEventListener('scroll', function() {
+  console.log(window.scrollY)
+  if(window.scrollY > 100) {
+      toTopBtn.style.display = "flex";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+})
